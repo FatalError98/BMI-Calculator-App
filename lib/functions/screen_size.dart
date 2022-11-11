2bf5b2ad64a8b80f3f0e56989562ffdc58442c93
Ammar Talal <103538665+FatalError98@users.noreply.github.com>
2022-11-11 12:56:50 +0300

    style: change final value to const
    Change the baseHeight final to const

--- lib/functions/screen_size.dart
+++ lib/functions/screen_size.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-final double baseHeight = 650.0;
+const double baseHeight = 650.0;
 double screenAwareSize(double size, BuildContext context) {
   return size * MediaQuery.of(context).size.longestSide / baseHeight;
 }
