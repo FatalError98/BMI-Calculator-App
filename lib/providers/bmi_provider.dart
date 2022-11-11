b034457358dc668cbce2e02ea92722a4f3849237
Ammar Talal <103538665+FatalError98@users.noreply.github.com>
2022-11-11 13:03:05 +0300

    build: add calculation logic

--- lib/providers/bmi_provider.dart
+++ lib/providers/bmi_provider.dart
@@ -1,8 +1,15 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 import '../models/bmi.dart';
 
 class BmiProvider with ChangeNotifier, Bmi {
+  String icon = '';
+  String category = '';
+  double bmi = 0.0;
+  double healthyWeightMax = 0.0;
+  double healthyWeightMin = 0.0;
+
   void decWeight() {
     if (weight > 10) {
       weight -= 0.1;
@@ -39,5 +46,37 @@ class BmiProvider with ChangeNotifier, Bmi {
   double weightSlider(double value) {
     notifyListeners();
     return weight = value;
+  }
+
+  void calculateBmiKgM() {
+    bmi = (weight / pow(height, 2)) * 10000;
+    healthyWeightMax = 25 * pow(height, 2) / 10000;
+    healthyWeightMin = 18.5 * pow(height, 2) / 10000;
+    if (bmi < 16) {
+      category = 'Severe Thinness';
+      icon = '😵';
+    } else if (bmi >= 16 && bmi <= 17) {
+      category = 'Moderate Thinness';
+      icon = '😱';
+    } else if (bmi >= 17 && bmi <= 18.5) {
+      category = 'Mild Thinness	';
+      icon = '😧';
+    } else if (bmi >= 18.5 && bmi <= 25) {
+      category = 'Normal';
+      icon = '🔥';
+    } else if (bmi >= 25 && bmi <= 30) {
+      category = 'Overweight';
+      icon = '😧';
+    } else if (bmi >= 30 && bmi <= 35) {
+      category = 'Obese Class I';
+      icon = '😨';
+    } else if (bmi >= 35 && bmi <= 40) {
+      category = 'Obese Class II';
+      icon = '😱';
+    } else if (bmi > 40) {
+      category = 'Obese Class III';
+      icon = '😵';
+    }
+    notifyListeners();
   }
 }
